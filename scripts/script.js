0efffde2a5e9da395b05479cdd430481ce12698c
Francisco Zuidwijk <electrocisko@gmail.com>
2023-12-07 19:31:03 -0300

    adding leap year

--- scripts/script.js
+++ scripts/script.js
@@ -6,17 +6,20 @@ const inputYearWarning = document.querySelector("#input-year-warning");
 const inputMonthWarning = document.querySelector("#input-month-warning");
 const inputDayWarning = document.querySelector("#input-day-warning");
 const inputDate = document.querySelectorAll(".input-date");
+const showYears = document.querySelector("#show-years");
+const showMonths = document.querySelector("#show-months");
+const showDays = document.querySelector("#show-days");
 
 const date2 = new Date(); // DATE2 IS TODAY
 console.log(date2);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   removeWarnings(); // clean all warnings before validate
+  let inputsOk=true;
   let data = new FormData(form);
   let day = data.get("day");
   let month = data.get("month");
   let year = data.get("year");
-
   //check the days in the month entered
   let daysInMonth = 31;
   if (month == 4 || month == 6 || month == 9 || month == 11) {
@@ -24,36 +27,47 @@ form.addEventListener("submit", (e) => {
   } else if (month == 2) {
     daysInMonth = 29;
   }
-
   if (!day) {
+    inputsOk=false;
     inputLabel[0].classList.add("warning");
     inputDate[0].classList.add("border-warning");
     inputWarning[0].innerHTML = "This field is required";
   } else if (day > daysInMonth || day < 1) {
+    inputsOk=false;
     addWarnings();
     inputDayWarning.innerHTML = "Must be a valid day";
   }
-
   if (!month) {
+    inputsOk=false;
     inputLabel[1].classList.add("warning");
     inputDate[1].classList.add("border-warning");
     inputWarning[1].innerHTML = "This field is required";
   } else if (month > 12 || month < 1) {
+    inputsOk=false;
     addWarnings();
     inputMonthWarning.innerHTML = "Must be in valid month";
   }
-
   if (!year) {
+    inputsOk=false;
     inputLabel[2].classList.add("warning");
     inputDate[2].classList.add("border-warning");
     inputWarning[2].innerHTML = "This field is required";
   } else if (year > date2.getFullYear() || year < 1) {  
+    inputsOk=false;
     addWarnings();
     inputYearWarning.innerHTML = "Must be in the past";
   }
-
   const date1 = new Date(year, parseInt(month) - 1, day);
-  calculateAge(date1);
+
+  if (inputsOk) {
+    let result = calculateAge(date1)
+    showDays.innerHTML=result.days;
+    showMonths.innerHTML=result.months;
+    showYears.innerHTML=result.years;
+  }
+
+  
+
 });
 
 function addWarnings() {
@@ -97,8 +111,7 @@ function calculateAge(date1) {
   if (monthPast == 3 || monthPast == 5 || monthPast == 8 || monthPast == 10)
     daysOfMonthPast = 30;
   if (monthPast == 1) {
-    daysOfMonthPast = 28;
-    //  Leap year missing
+    leapYear(year1)?  daysOfMonthPast = 29 :  daysOfMonthPast = 28;
   }
 
   let diffMonth = month2 - month1;
@@ -114,7 +127,7 @@ function calculateAge(date1) {
     } else if (day1 < day2) {
       days = day2 - day1;
     }
-    console.log({
+    return({
       years,
       months,
       days,
@@ -130,7 +143,7 @@ function calculateAge(date1) {
       months = months - 1;
       days = daysOfMonthPast - day2 + day1;
     }
-    console.log({
+    return({
       years,
       months,
       days,
@@ -142,8 +155,7 @@ function calculateAge(date1) {
     if (monthPast == 3 || monthPast == 5 || monthPast == 8 || monthPast == 10)
       daysOfMonthPast = 30;
     if (monthPast == 1) {
-      daysOfMonthPast = 28;
-      //  Leap year missing
+      leapYear(year1)?  daysOfMonthPast = 29 :  daysOfMonthPast = 28;
     }
     let days = 0;
     if (diffDay < 0) {
@@ -151,7 +163,7 @@ function calculateAge(date1) {
       const years = year2 - year1 - 1;
       const months = 11;
       days = daysOfMonthPast - day1 + day2;
-      console.log({
+      return({
         years,
         months,
         days,
@@ -161,7 +173,7 @@ function calculateAge(date1) {
       const years = year2 - year1;
       const months = 0;
       days = day2 - day1;
-      console.log({
+      return({
         years,
         months,
         days,
@@ -170,7 +182,7 @@ function calculateAge(date1) {
       // birthday is today
       const years = year2 - year1;
       const months = 0;
-      console.log({
+      return({
         years,
         months,
         days,
@@ -178,3 +190,13 @@ function calculateAge(date1) {
     }
   }
 }
+
+
+function leapYear (y) {
+  if( (y%400 == 0) ||  (y%4 == 0 && y%100 != 0)) {
+    return true
+  } else return false;
+}
+
+
+
